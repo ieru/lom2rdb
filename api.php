0b24f1b5c2ec1e8cc77c678e7019a3bf41e19794
David Baños <maketas@gmail.com>
2013-07-03 01:34:35 +0200

    Changes

--- api.php
+++ api.php
@@ -3,11 +3,6 @@
 // after cloning the project from Github
 require_once( 'vendor/autoload.php' );
 
-// Allowed domain for cross domain javascript requests
-// Please, change * for your server name, including http://
-// It distinguises between www.yourserver.com and yourserver.com
-define( 'XDOMAIN_ALLOWED_SERVER', '*' );
-
 // Remove error reporting (uncomment in production environment)
 //error_reporting(0);
 
